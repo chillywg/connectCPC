b6ac559a3f1bfb545c08d47a1928096ea71e2063
zcs153132275 <153132275@qq.com>
2025-10-17 14:41:15 +0800

    修改

--- src/main/java/com/shunchao/config/CpcPathConfig.java
+++ src/main/java/com/shunchao/config/CpcPathConfig.java
@@ -15,19 +15,19 @@ public class CpcPathConfig {
         CpcPathInComputer cpcPathInComputer = new CpcPathInComputer();
         try {
             //参数后期写入配置文件 TODO
-            JExecResult result = JRegistry.query("HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\gwssi\\CPC客户端",
-                    new JQueryOptions().useF("\"CPC\""));
-            log.info(result.toString());
-            String client = result.getLines()[1];
-            String data = result.getLines()[2];
-            String clientPath = client.split("REG_SZ")[1].trim();
-            String dataPath = data.split("REG_SZ")[1].trim();
-            CpcPathInComputer.setClientCpcPathWindowsComputer(clientPath);
-            CpcPathInComputer.setCpcDataPathWindowsComputer(dataPath);
-            CpcPathInComputer.setCpcBinPathWindowsComputer(clientPath.substring(0,clientPath.lastIndexOf("\\")));
-
-            log.info(clientPath.substring(0,clientPath.lastIndexOf("\\")));
-            log.info(clientPath);
+//            JExecResult result = JRegistry.query("HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\gwssi\\CPC客户端",
+//                    new JQueryOptions().useF("\"CPC\""));
+//            log.info(result.toString());
+//            String client = result.getLines()[1];
+//            String data = result.getLines()[2];
+//            String clientPath = client.split("REG_SZ")[1].trim();
+//            String dataPath = data.split("REG_SZ")[1].trim();
+//            CpcPathInComputer.setClientCpcPathWindowsComputer(clientPath);
+//            CpcPathInComputer.setCpcDataPathWindowsComputer(dataPath);
+//            CpcPathInComputer.setCpcBinPathWindowsComputer(clientPath.substring(0,clientPath.lastIndexOf("\\")));
+//
+//            log.info(clientPath.substring(0,clientPath.lastIndexOf("\\")));
+//            log.info(clientPath);
             /*System.out.println(clientPath.substring(0,clientPath.lastIndexOf("\\")));
             System.out.println(clientPath);*/
         } catch (Exception e) {
